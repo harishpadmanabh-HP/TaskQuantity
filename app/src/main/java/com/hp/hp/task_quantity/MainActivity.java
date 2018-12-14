385b8ed11e24e95d873d83fe9b215da3451bb139
harishpadmanabh-HP <harishpadmanabh.sics@gmail.com>
2018-12-14 23:52:43 +0530

    number pickr added

--- app/src/main/java/com/hp/hp/task_quantity/MainActivity.java
+++ app/src/main/java/com/hp/hp/task_quantity/MainActivity.java
@@ -1,23 +1,28 @@
 package com.hp.hp.task_quantity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
-
+    Dialog d;
+    NumberPicker numberPicker;
+    TextView quant;
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- final TextView quant=findViewById(R.id.quant);
+  quant=findViewById(R.id.quant);
         final TextView fare=findViewById(R.id.textView6);
 
 
@@ -58,6 +63,54 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,DisplayBill.class));
             }
         });
+       quant.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               show();
+           }
+       });
+
+    }
+
+    public void show()
+    {
+        final Dialog d = new Dialog(MainActivity.this);
+        d.setTitle("NumberPicker");
+        d.setContentView(R.layout.numpick);
+        Button b1 = (Button) d.findViewById(R.id.button);
+        Button b2 = (Button) d.findViewById(R.id.button2);
+        final NumberPicker np = (NumberPicker) d.findViewById(R.id.num);
+        np.setMaxValue(100); // max value 100
+        np.setMinValue(0);   // min value 0
+        np.setWrapSelectorWheel(false);
+        //np.setOnValueChangedListener();
+
+//      np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+//          @Override
+//          public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+//              Toast.makeText(MainActivity.this, ""+ Integer.toString(i1), Toast.LENGTH_SHORT).show();
+//
+//          }
+//      });
+        b1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                quant.setText(String.valueOf(np.getValue())); //set the value to textview
+                d.dismiss();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                d.dismiss(); // dismiss the dialog
+            }
+        });
+
+
+        d.show();
+
 
     }
 
